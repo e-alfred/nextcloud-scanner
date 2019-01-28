df5f5145bd01561bd018c62c041110b7e3f18248
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2019-01-28 15:38:24 +0100

    Rework scan parameter handling. Introduce new classes that deal with the different parameter types. As a result, pass more valuable information to the frontend.

---
+++ lib/Sane/Param/NullScanParam.php
@@ -0,0 +1,33 @@
+<?php # -*- coding: utf-8 -*-
+declare(strict_types=1);
+
+
+namespace OCA\Scanner\Sane\Param;
+
+
+class NullScanParam implements ScanParam {
+
+	public function accepts(string $value): bool {
+		return false;
+	}
+
+	public function options(): array {
+		return [];
+	}
+
+	public function defaultValue(): string {
+		return '';
+	}
+
+	public function description(): string {
+		return '';
+	}
+
+	public function type(): string {
+		return '';
+	}
+
+	public function name(): string {
+		return '';
+	}
+}

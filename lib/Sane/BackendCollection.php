67d518b8d87ddff43e7296adfc342d8288355f55
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2020-01-28 12:48:35 +0100

    Fix possible fatal error

--- lib/Sane/BackendCollection.php
+++ lib/Sane/BackendCollection.php
@@ -5,52 +5,62 @@ declare(strict_types=1);
 namespace OCA\Scanner\Sane;
 
 
-class BackendCollection {
-	/**
-	 * @var SaneBackend[]
-	 */
-	private $saneBackends;
+class BackendCollection
+{
+    /**
+     * @var SaneBackend[]
+     */
+    private $saneBackends;
 
-	public function __construct(array $saneBackends) {
+    public function __construct(array $saneBackends)
+    {
 
-		$this->saneBackends = $saneBackends;
-	}
+        $this->saneBackends = $saneBackends;
+    }
 
-	public static function fromShell(): BackendCollection {
-		exec('scanimage -L', $lines);
-		return self::fromShellOutput($lines);
-	}
+    public static function fromShell(): BackendCollection
+    {
+        exec('scanimage -L', $lines);
+        return self::fromShellOutput($lines);
+    }
 
-	public static function fromShellOutput(array $shellOutput): BackendCollection {
-		$backendIds = [];
-		foreach ($shellOutput as $line) {
-			preg_match('/`(.*)\'/', $line, $matches);
-			$backendIds[] = $matches[1];
-		}
-		$backends = [];
-		foreach ($backendIds as $backendId) {
-			try {
-				$backend = SaneBackend::fromShell($backendId);
-				$backends[$backendId] = $backend;
-			} catch (Exception\InvalidArgumentException $e) {
-				continue;
-			}
-		}
-		return new self($backends);
-	}
+    public static function fromShellOutput(array $shellOutput): BackendCollection
+    {
+        $backendIds = [];
+        foreach ($shellOutput as $line) {
+            preg_match('/`(.*)\'/', $line, $matches);
+            if (!$matches[1]) {
+                continue;
+            }
+            $backendIds[] = $matches[1];
+        }
+        $backends = [];
+        foreach ($backendIds as $backendId) {
+            try {
+                $backend = SaneBackend::fromShell($backendId);
+                $backends[$backendId] = $backend;
+            } catch (Exception\InvalidArgumentException $e) {
+                continue;
+            }
+        }
+        return new self($backends);
+    }
 
-	public function getAll(): array {
-		return $this->saneBackends;
-	}
+    public function getAll(): array
+    {
+        return $this->saneBackends;
+    }
 
-	public function getByIndex(int $idx): SaneBackend {
-		$indexed = array_values($this->saneBackends);
-		return $indexed[$idx];
-	}
+    public function getByIndex(int $idx): SaneBackend
+    {
+        $indexed = array_values($this->saneBackends);
+        return $indexed[$idx];
+    }
 
-	public function toArray(): array {
-		return array_values(array_map(function (SaneBackend $backend) {
-			return $backend->toArray();
-		}, $this->saneBackends));
-	}
+    public function toArray(): array
+    {
+        return array_values(array_map(function (SaneBackend $backend) {
+            return $backend->toArray();
+        }, $this->saneBackends));
+    }
 }
